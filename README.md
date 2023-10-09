66cf62dd7a9fc9dbc3ba403ef67afce58a210542
Dave O'Connor <git@mail.andvari.net>
2023-10-09 15:27:14 +0100

    Initial getting of gcloud APi working and docs on setup.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # clouddns-sync
+
+Utlity for syncing local DNS records to gcloud (and back).
+
+# Getting Started
+
+Assuming yu have the `gcloud` command set up with access to Google Cloud console, the following should get you going:
+
+(Figuring out the clicky web console equivalent is left as an exercise to the reader etc. etc.)
+
+```
+# Create a nice project just for dns
+gcloud projects create mydnsproject
+
+# Switch to the new project
+gcloud config set project mydnsproject
+
+# Enable Cloud DNS for this project
+gcloud services enable dns.googleapis.com
+
+# Set up your first zone.
+gcloud dns managed-zones create myzone --dns-name=myzone.mydomain.tld --description="My Zone"
+
+# Set up a service account to admininster just DNS
+gcloud iam service-accounts create gcloud-dns-sync
+
+# Add your service account to the DNS Administrator role for this project
+gcloud projects add-iam-policy-binding myproject  --member=serviceAcount:cloud-dns-sync@mydnsproject.iam.gserviceaccount.com --role=roles/dns.admin
+
+# Generate a key for the service account. There's a more secure way of doing this with identity federation doodly do but who can be arsed.
+gcloud iam service-accounts keys create cloud-dns-sync.key.json --key-file-type=json --iam-account=cloud-dns-sync@mydnsproject.iam.gserviceaccount.com
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+				"os"
+				"context"
+				"google.golang.org/api/option"
+				"fmt"
+				"io/ioutil"
+				"flag"
+				google_oauth "golang.org/x/oauth2/google"
+				"google.golang.org/api/dns/v1"
+			)
+
+func getCloudManagedZones(dnsservice *dns.Service, project string) ([]*dns.ManagedZone, error) {
+	nextPageToken := ""
+	ret := []*dns.ManagedZone{}
+	for {
+		out, err := dnsservice.ManagedZones.List(project).PageToken(nextPageToken).Do()
+		if err != nil {
+			return ret, err
+		}
+		ret = append(ret, out.ManagedZones...)
+		if out.NextPageToken == "" {
+			break
+		}
+		nextPageToken = out.NextPageToken
+	}
+	return ret, nil
+}
+
+
+func getResourceRecordSetsForZone(dnsservice *dns.Service, project *string, zone *string) ([]*dns.ResourceRecordSet, error) {
+	nextPageToken := ""
+	ret := []*dns.ResourceRecordSet{}
+	for {
+		fmt.Println("get")
+		out, err := dnsservice.ResourceRecordSets.List(*project, *zone).PageToken(nextPageToken).Do()
+		if err != nil {
+			fmt.Println("whoops")
+			return ret, err
+		}
+		ret = append(ret, out.Rrsets...)
+		if out.NextPageToken == "" {
+			break
+		}
+		nextPageToken = out.NextPageToken
+	}
+	return ret, nil
+}
+
+
+func main() {
+	var jsonKeyfile = flag.String("json-keyfile", "key.json", "json credentials file for Cloud DNS")
+	var cloudProject = flag.String("cloud-project", "myproject", "Google Cloud Project")
+	var cloudZone = flag.String("cloud-dns-zone", "myzone", "Cloud DNS zone to operate on")
+	flag.Parse()
+
+	jsonData, ioerror := ioutil.ReadFile(*jsonKeyfile)
+	if ioerror != nil {
+		fmt.Println(*jsonKeyfile, ioerror)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+
+	creds, err := google_oauth.CredentialsFromJSON(ctx, jsonData, "https://www.googleapis.com/auth/cloud-platform")
+
+	if err != nil {
+		fmt.Println("Cloud DNS Error: ", err)
+		os.Exit(1)
+	}
+
+	dnsservice, err := dns.NewService(ctx, option.WithCredentials(creds))
+
+	if err != nil {
+		fmt.Println("Cloud DNS Error: ", err)
+		os.Exit(1)
+	}
+
+	//zones, err := getCloudManagedZones(dnsservice, "awaylab")
+	rrs, err := getResourceRecordSetsForZone(dnsservice, cloudProject, cloudZone)
+
+	if err != nil {
+		fmt.Println("Cloud DNS Error: ", err.Error())
+	}
+
+	for _, z := range rrs {
+		fmt.Println(z.Name, ": ", z.Type)
+	}
+
+}
+
