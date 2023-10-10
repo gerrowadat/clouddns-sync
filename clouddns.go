8748949ac05334911beb8cb4c67be695fcc51a34
Dave O'Connor <git@mail.andvari.net>
2023-10-11 00:46:48 +0100

    Move cloud Dns utility functions to clouddns.go

---
+++ clouddns.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"google.golang.org/api/dns/v1"
+)
+
+func getCloudManagedZones(dnsservice *dns.Service, project *string) ([]*dns.ManagedZone, error) {
+	nextPageToken := ""
+	ret := []*dns.ManagedZone{}
+	for {
+		out, err := dnsservice.ManagedZones.List(*project).PageToken(nextPageToken).Do()
+		if err != nil {
+			return ret, err
+		}
+		ret = append(ret, out.ManagedZones...)
+		if out.NextPageToken == "" {
+			break
+		}
+		nextPageToken = out.NextPageToken
+	}
+	return ret, nil
+}
+
+func getResourceRecordSetsForZone(dnsservice *dns.Service, project *string, zone *string) ([]*dns.ResourceRecordSet, error) {
+	nextPageToken := ""
+	ret := []*dns.ResourceRecordSet{}
+
+	for {
+		call := dnsservice.ResourceRecordSets.List(*project, *zone)
+
+		if nextPageToken != "" {
+			call = call.PageToken(nextPageToken)
+		}
+
+		out, err := call.Do()
+
+		if err != nil {
+			return ret, err
+		}
+
+		ret = append(ret, out.Rrsets...)
+
+		if out.NextPageToken == "" {
+			break
+		}
+
+		nextPageToken = out.NextPageToken
+	}
+
+	return ret, nil
+}
+
+func ZoneFileFragment(rr *dns.ResourceRecordSet) string {
+	ret := []string{}
+	if rr.Type != "SOA" {
+		soa_str := string("")
+		if int(rr.Ttl) != 0 {
+			soa_str = fmt.Sprintf("%s ", strconv.Itoa(int(rr.Ttl)))
+		}
+		for i, _ := range rr.Rrdatas {
+			ret = append(ret, fmt.Sprintf("%s %sIN %s %s", rr.Name, soa_str, rr.Type, string(rr.Rrdatas[i])))
+		}
+	} else {
+		// SOA
+		ret = append(ret, fmt.Sprintf("%s IN %s %s", rr.Name, rr.Type, string(rr.Rrdatas[0])))
+	}
+	return strings.Join(ret, "\n")
+}
--- main.go
+++ main.go
@@ -6,76 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	google_oauth "golang.org/x/oauth2/google"
 	"google.golang.org/api/dns/v1"
 	"google.golang.org/api/option"
 )
-
-func getCloudManagedZones(dnsservice *dns.Service, project *string) ([]*dns.ManagedZone, error) {
-	nextPageToken := ""
-	ret := []*dns.ManagedZone{}
-	for {
-		out, err := dnsservice.ManagedZones.List(*project).PageToken(nextPageToken).Do()
-		if err != nil {
-			return ret, err
-		}
-		ret = append(ret, out.ManagedZones...)
-		if out.NextPageToken == "" {
-			break
-		}
-		nextPageToken = out.NextPageToken
-	}
-	return ret, nil
-}
-
-func getResourceRecordSetsForZone(dnsservice *dns.Service, project *string, zone *string) ([]*dns.ResourceRecordSet, error) {
-	nextPageToken := ""
-	ret := []*dns.ResourceRecordSet{}
-
-	for {
-		call := dnsservice.ResourceRecordSets.List(*project, *zone)
-
-		if nextPageToken != "" {
-			call = call.PageToken(nextPageToken)
-		}
-
-		out, err := call.Do()
-
-		if err != nil {
-			return ret, err
-		}
-
-		ret = append(ret, out.Rrsets...)
-
-		if out.NextPageToken == "" {
-			break
-		}
-
-		nextPageToken = out.NextPageToken
-	}
-
-	return ret, nil
-}
-
-func ZoneFileFragment(rr *dns.ResourceRecordSet) string {
-	ret := []string{}
-	if rr.Type != "SOA" {
-		soa_str := string("")
-		if int(rr.Ttl) != 0 {
-			soa_str = fmt.Sprintf("%s ", strconv.Itoa(int(rr.Ttl)))
-		}
-		for i, _ := range rr.Rrdatas {
-			ret = append(ret, fmt.Sprintf("%s %sIN %s %s", rr.Name, soa_str, rr.Type, string(rr.Rrdatas[i])))
-		}
-	} else {
-		// SOA
-		ret = append(ret, fmt.Sprintf("%s IN %s %s", rr.Name, rr.Type, string(rr.Rrdatas[0])))
-	}
-	return strings.Join(ret, "\n")
-}
 
 func main() {
 	var jsonKeyfile = flag.String("json-keyfile", "key.json", "json credentials file for Cloud DNS")
